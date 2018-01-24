80c47adb5231a4714cd1e415140ccbdca26503fd
Armando Longart <ajzlongart@gmail.com>
2018-01-24 19:26:08 -0400

    Update ColorRestoration_SimpleColorBalanceHSV.py
    Modificación de Código: Eliminación del análisis cuantitativo y detalles menores

--- ColorRestoration_SimpleColorBalanceHSV.py
+++ ColorRestoration_SimpleColorBalanceHSV.py
@@ -20,7 +20,9 @@ Variacion del Simple Color Balance (en RGB) de DavidYKay: https://gist.github.co
 import cv2
 import math
 import numpy as np
+#import sys
 from matplotlib import pyplot as plt
+#from matplotlib import colors
 import argparse
 import os
 
@@ -102,7 +104,7 @@ def sColorBalance(img_hsv, porcentaje):
 		norm_tile = np.tile(normalized[:,:,np.newaxis],(1,1,3))
 	
 		
-	return cv2.merge(salida_canales)
+	return cv2.merge(salida_canales)	#norm_tile
 
 
 if __name__ == '__main__':
@@ -113,9 +115,6 @@ if __name__ == '__main__':
 
 	imgOriginal = cv2.imread(args["image"])
 
-	#-------------------Creacion del archivo--------------------------
-	f = open('img_txt.txt','a') #Tambien sirve open('img_txt.txt')  Archivo para colocar los resultados de los analisis cuantitativos. Sera append
-
 	img_hsv = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV) 		#Conversion de HSV de la imagen original a HSV
 
 	#-----Llamado a Funcion----------------------------------------------------
@@ -123,135 +122,62 @@ if __name__ == '__main__':
 	imgRecuperadaRGB = cv2.cvtColor(imgRecuperada,cv2.COLOR_HSV2BGR)	#Conversion de HSV de la imagen recuperada a RGB
 
 	#-----Resultados----------------------------------------------------
-	cv2.imshow("imgOriginal", imgOriginal)
-	cv2.imshow("imgRecuperadaHSV", imgRecuperada)
+#	cv2.imshow("imgOriginal", imgOriginal)
+#	cv2.imshow("imgRecuperadaHSV", imgRecuperada)
 	cv2.imshow("imgRecuperada", imgRecuperadaRGB)
 
 	#-----Guardado de la imagen Recuperada-------------------------------------------
-	cv2.imwrite('imagenRecuperadaCR_HSV_RGB.jpg',imgRecuperadaRGB)
+	cv2.imwrite(args["image"]+"CR_RGB.jpg", imgRecuperadaRGB)
 
 	#-----Calculo de Histograma----------------------------------------------------
 	'''
 	Se calcula el histograma de la imagen Original en HSV para conocer el rango para aplicar
 	el algoritmo
 	'''
-	hue,sat,val = cv2.split(img_hsv) 
-	
-	plt.subplot(311)                             #plot in the first cell
-	plt.subplots_adjust(hspace=.5)
-	plt.title("Hue Original")
-	plt.ylabel('Numero de Pixeles')
-	plt.hist(np.ndarray.flatten(hue), bins=128)
-	plt.xlim([0,180])
-
-	plt.subplot(312)                             #plot in the second cell
-	plt.title("Saturation Original")
-	plt.ylabel('Numero de Pixeles')
-	plt.hist(np.ndarray.flatten(sat), bins=128)
-	plt.xlim([0,256])
-
-	plt.subplot(313)                             #plot in the third cell
-	plt.title("Luminosity Value Original")
-	plt.ylabel('Numero de Pixeles')
-	plt.hist(np.ndarray.flatten(val), bins=128)
-	plt.xlim([0,256])
-
-	plt.show()
-
-	'''
-	Aqui se calcula el histograma de la imagen Original y la Recuperada en RGB
-	'''
-	color = ('b','g','r')
-	for i, col in enumerate(color):
-	   histcolorOriginal =  cv2.calcHist([imgOriginal],[i],None,[256],[0,256])
-	   histcolorRecuperadaRGB =  cv2.calcHist([imgRecuperadaRGB],[i],None,[256],[0,256])
-
-	   plt.subplot(211), plt.plot(histcolorOriginal,color=col)
-	   plt.title('Histograma Original')
-	   plt.ylabel('Numero de Pixeles')
-	   plt.xlim([0,256])
-
-	   plt.subplot(212), plt.plot(histcolorRecuperadaRGB,color=col)
-	   plt.title('Histograma Recuperada')
-	   plt.ylabel('Numero de Pixeles')
-	   plt.xlabel('Bins')
-	   plt.xlim([0,256])
-
-	plt.show()
-
-	#-----Analisis Cuantitativo de la Imagen------------------------------------------------
-	'''
-	Se realizan 3 tipos de analisis de la imagen resultante:
-	Espectro Frecuencial
-	Entropia
-	Deteccion de features
-	Con la finalidad de saber cual es el algoritmo que mejor funciona para las imagenes
-	submarinas
-	'''
-
-	#Espectro Frecuencial
-	IMG = cv2.imread(args["image"],0)
-	IMGRec = cv2.imread('imagenRecuperadaCR_HSV_RGB.jpg',0)
-#	IMG = cv2.resize(IMG, (1024, 768))
-#	IMGRec = cv2.resize(IMGRec, (1024, 768))
-	img32 = np.float32(IMG)
-	imgRec32 = np.float32(IMGRec)
-
-	row,col = np.shape(img32)
-
-	fourier32 = np.fft.fft2(img32)/float(row*col)
-	fourierShift32 = np.fft.fftshift(fourier32)
-	mod_fourier32 = np.abs(fourierShift32)
-
-	max_mod_fourier32 = np.max(mod_fourier32)
-	thresh32 = max_mod_fourier32/1000
-	thresh_fourier32 = mod_fourier32[(mod_fourier32>thresh32)]	#*mod_fourier
-	tam_thresh_fourier32 = np.size(thresh_fourier32)
-
-	iqm32 = tam_thresh_fourier32/(float(row*col))
-
-	fourierRec32 = np.fft.fft2(imgRec32)/float(row*col)
-	fourierShiftRec32 = np.fft.fftshift(fourierRec32)
-	mod_fourierRec32 = np.abs(fourierShiftRec32)
-
-	max_mod_fourierRec32 = np.max(mod_fourierRec32)
-	threshRec32 = max_mod_fourierRec32/1000
-	thresh_fourierRec32 = mod_fourierRec32[(mod_fourierRec32>threshRec32)]	#*mod_fourier
-	tam_thresh_fourierRec32 = np.size(thresh_fourierRec32)
-
-	iqmRec32 = tam_thresh_fourierRec32/(float(row*col))
-
-	print iqm32
-	print iqmRec32
-
-	plt.subplot(311),plt.imshow(img32,cmap = 'gray')
-	plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-	plt.subplot(312),plt.imshow(20*np.log(mod_fourier32))
-	plt.title('Magnitude Spectrum Original'), plt.xticks([]), plt.yticks([])
-	plt.subplot(313),plt.imshow(20*np.log(mod_fourierRec32))
-	plt.title('Magnitude Spectrum Rec'), plt.xticks([]), plt.yticks([])
-	plt.show()
-
-
-	#Entropia de la imagen a partir del histograma de grises de la iamgen
-	histogramIMG = cv2.calcHist([IMG],[0],None,[256],[0,256])
-	cv2.normalize(histogramIMG,histogramIMG,alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-	histIMG = histogramIMG.sum()
-	probIMG = [float(h)/histIMG for h in histogramIMG]
-	entropyIMG = -np.sum([p*np.log2(p) for p in probIMG if p !=0])
-	print entropyIMG
-
-	histogramIMGRec = cv2.calcHist([IMGRec],[0],None,[256],[0,256])
-	cv2.normalize(histogramIMGRec,histogramIMGRec,alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-	histIMGRec = histogramIMGRec.sum()
-	probIMGRec = [float(h)/histIMGRec for h in histogramIMGRec]
-	entropyIMGRec = -np.sum([p*np.log2(p) for p in probIMGRec if p !=0])
-	print entropyIMGRec
-
-	#-----Escritura del archivo con los resultados----------------------------------------------
-	#Con write()
-	f.write('%s \t %d \t %d \t %f \t %f \t %f \t %f \t HSVStretch \n' %(args["image"], row, col, iqm32, iqmRec32, entropyIMG, entropyIMGRec))
-	f.close()
+#	hue,sat,val = cv2.split(img_hsv) 
+#	
+#	plt.subplot(311)                             #plot in the first cell
+#	plt.subplots_adjust(hspace=.5)
+#	plt.title("Hue Original")
+#	plt.ylabel('Numero de Pixeles')
+#	plt.hist(np.ndarray.flatten(hue), bins=128)
+#	plt.xlim([0,180])
+#
+#	plt.subplot(312)                             #plot in the second cell
+#	plt.title("Saturation Original")
+#	plt.ylabel('Numero de Pixeles')
+#	plt.hist(np.ndarray.flatten(sat), bins=128)
+#	plt.xlim([0,256])
+#
+#	plt.subplot(313)                             #plot in the third cell
+#	plt.title("Luminosity Value Original")
+#	plt.ylabel('Numero de Pixeles')
+#	plt.hist(np.ndarray.flatten(val), bins=128)
+#	plt.xlim([0,256])
+#
+#	plt.show()
+#
+#	'''
+#	Aqui se calcula el histograma de la imagen Original y la Recuperada en RGB
+#	'''
+#	color = ('b','g','r')
+#	for i, col in enumerate(color):
+#	   histcolorOriginal =  cv2.calcHist([imgOriginal],[i],None,[256],[0,256])
+#	   histcolorRecuperadaRGB =  cv2.calcHist([imgRecuperadaRGB],[i],None,[256],[0,256])
+#
+#	   plt.subplot(211), plt.plot(histcolorOriginal,color=col)
+#	   plt.title('Histograma Original')
+#	   plt.ylabel('Numero de Pixeles')
+#	   plt.xlim([0,256])
+#
+#	   plt.subplot(212), plt.plot(histcolorRecuperadaRGB,color=col)
+#	   plt.title('Histograma Recuperada')
+#	   plt.ylabel('Numero de Pixeles')
+#	   plt.xlabel('Bins')
+#	   plt.xlim([0,256])
+#
+#	plt.show()
 
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+
